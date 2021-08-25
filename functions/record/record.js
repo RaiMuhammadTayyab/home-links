2b98ede06d2a860d46ed9588197e0d950c1c3b84
RaiMuhammadTayyab <66311583+RaiMuhammadTayyab@users.noreply.github.com>
2021-08-25 22:33:21 +0500

    Second day commit
    just added card component

---
+++ functions/record/record.js
@@ -0,0 +1,85 @@
+const { ApolloServer, gql } = require('apollo-server-lambda')
+const shortid = require('shortid')
+const faunadb = require('faunadb')
+  q = faunadb.query
+const typeDefs = gql`
+  type Query {
+   person:[record]
+  }
+
+  type Mutation {
+    addperson(
+      name:String,
+      title:String
+    ):record
+  }
+  type record {
+    id:  ID!
+    name: String!
+    title:String!
+    Link:String!
+  }
+`
+const resolvers = {
+  Query: {
+    person:async(root,args,context)=>{
+      try{
+var adminClient=new faunadb.Client({secret:'fnAERNN6VmACQrN3xcoHwWuQfBeG2cTe5RBLWpOu'})
+const result= await adminClient.query(
+q.Map(
+  q.Paginate(
+    q.Match(
+      q.Index('links'), ) ),
+  q.Lambda(x=>q.Get(x))
+  )
+)
+  return result.data.map(d=>{
+    return {
+      title:d.data.title,
+      name:d.data.name,
+      id:d.ts,
+      Link:d.data.Link
+
+  }})
+}
+    
+catch(error){
+  console.log(error)
+
+}
+   }
+},
+  
+Mutation: {
+  addperson: async (_,{name,title})=>{
+    try{
+var adminClient=new faunadb.Client({secret:"fnAERNN6VmACQrN3xcoHwWuQfBeG2cTe5RBLWpOu"})
+const result= await adminClient.query(
+  q.Create(
+    q.Collection('link'),
+    {
+     data:{
+       name,
+       title,
+      Link: shortid.generate()
+     }  
+    },
+  )
+)
+return result.data.data 
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const handler = server.createHandler()
+module.exports = { handler }
---
+++ src/pages/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Card=(location )=>{
+
+    return(
+<p>hello world</p>)
+
+}
+export default Card
